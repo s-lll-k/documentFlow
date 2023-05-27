fee5b8fefdea72a7425afeaacbfd17b45f922684
s-lll-k <sylik3210@gmail.com>
2023-05-27 14:41:25 +0600

    add calendar page

---
+++ pages/calendar/index.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  async created() {
+  },
+};
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.swiper {
+  margin-bottom: 60px;
+}
+.swiper-item {
+  max-width: 325px;
+  width: 100%;
+  height: 193px;
+  border-radius: 8px;
+  background: white;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 32px;
+    color: #0c0b0b;
+    margin-top: 18px;
+  }
+}
+</style>
