a3c5d001c87eee6298b0a20f4fb7b0f9cef42ba1
s-lll-k <sylik3210@gmail.com>
2023-06-01 22:28:12 +0600

    add calendar

--- pages/calendar/index.vue
+++ pages/calendar/index.vue
@@ -5,9 +5,324 @@ export default {
   data() {
     return {
       monthTitle: '',
-      daysOfWeek: ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'],
+      daysOfWeek: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
       weeks: [],
-      selectedDate: '2023-05-01',
+      events: 0,
+      weekend: 0,
+      selectedDates: ['2023-01-01', '2023-02-01', '2023-03-01', '2023-04-01', '2023-05-01', '2023-06-01'],
+      calendarMonths: [],
+      calendarEvents: [
+        {
+          date: '2023-01-01',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-07',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-14',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-15',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-21',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-22',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-23',
+          event: true,
+          name: 'Начало семестра'
+        },
+        {
+          date: '2023-01-28',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-01-29',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-04',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-05',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-07',
+          event: true,
+          name: 'Благотворительная ярмарка'
+        },
+        {
+          date: '2023-02-11',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-12',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-13',
+          event: true,
+          name: 'Защита преддипломной практики'
+        },
+        {
+          date: '2023-02-18',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-19',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-20',
+          event: true,
+          name: 'Выставление оценок преддипломной практики'
+        },
+        {
+          date: '2023-02-25',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-02-26',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-04',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-05',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-08',
+          event: true,
+          name: 'Международный женский день'
+        },
+        {
+          date: '2023-03-11',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-12',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-18',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-19',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-21',
+          event: true,
+          name: 'Наурыз'
+        },
+        {
+          date: '2023-03-22',
+          event: true,
+          name: 'Наурыз'
+        },
+        {
+          date: '2023-03-23',
+          event: true,
+          name: 'Наурыз'
+        },
+        {
+          date: '2023-03-25',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-03-26',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-01',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-02',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-05',
+          event: true,
+          name: 'Ярмарка вакансий'
+        },
+        {
+          date: '2023-04-08',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-09',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-15',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-16',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-20',
+          event: true,
+          name: 'Регистрация на элективные дисциплины'
+        },
+        {
+          date: '2023-04-22',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-23',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-29',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-04-30',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-02',
+          event: true,
+          name: 'Проверка дипломных проектов на плагиат'
+        },
+        {
+          date: '2023-05-06',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-07',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-13',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-14',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-15',
+          event: true,
+          name: 'Начало сессии'
+        },
+        {
+          date: '2023-05-20',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-21',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-28',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-05-29',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-03',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-04',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-10',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-11',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-12',
+          event: true,
+          name: 'Защита дипломных проектов'
+        },
+        {
+          date: '2023-06-17',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-18',
+          event: false,
+          name: 'Выходной'
+        },
+        {
+          date: '2023-06-19',
+          event: false,
+          name: 'Выходной'
+        }
+      ],
       swiperOptions: {
         slidesPerView: 1,
         spaceBetween: 20,
@@ -25,53 +340,100 @@ export default {
     };
   },
   async created() {
-    this.generateCalendar();
+    this.generateCalendar(this.selectedDates);
   },
   methods: {
-    generateCalendar() {
-      const date = new Date(this.selectedDate);
-      const year = date.getFullYear();
-      const month = date.getMonth();
-      
-      const firstDay = new Date(year, month, 1);
-      const lastDay = new Date(year, month + 1, 0);
-      const numDays = lastDay.getDate();
-      const startingDay = firstDay.getDay();
-      
-      const weeks = [];
-      let dayCounter = 1;
-      
-      for (let i = 0; i < 6; i++) {
-        const week = [];
+    generateCalendar(months) {
+      months.forEach(selectedMonth => {
+        const date = new Date(selectedMonth);
+        const year = date.getFullYear();
+        const month = date.getMonth();
         
-        if (i === 0) {
-          for (let j = 0; j < 7; j++) {
-            if (j >= startingDay) {
-              week.push(dayCounter);
-              dayCounter++;
-            } else {
-              week.push(null);
-            }
-          }
-        } else {
-          for (let j = 0; j < 7; j++) {
-            if (dayCounter <= numDays) {
-              week.push(dayCounter);
-              dayCounter++;
-            } else {
-              week.push(null);
-            }
+        const firstDay = new Date(year, month, 1);
+        const lastDay = new Date(year, month + 1, 0);
+        const numDays = lastDay.getDate();
+        
+        const weeks = [];
+        let week = [];
+        let dayCounter = 1;
+
+        // Определяем первый день недели (понедельник)
+        let startingDay = firstDay.getDay();
+        startingDay = (startingDay === 0) ? 6 : startingDay - 1;
+
+        // Добавляем пустые ячейки для первой недели до начального дня
+        for (let i = 0; i < startingDay; i++) {
+          week.push(null);
+        }
+
+        // Заполняем дни месяца
+        for (let i = 0; i < numDays; i++) {
+          week.push(dayCounter);
+          dayCounter++;
+
+          // Проверяем конец недели
+          if (week.length === 7) {
+            weeks.push(week);
+            week = [];
           }
         }
-        
-        weeks.push(week);
-        if (dayCounter > numDays) {
-          break;
+
+        // Добавляем пустые ячейки для последней недели после конечного дня
+        if (week.length > 0) {
+          while (week.length < 7) {
+            week.push(null);
+          }
+          weeks.push(week);
         }
-      }
-      const monthName = date.toLocaleString('ru-RU', { month: 'long' });
-      this.monthTitle = monthName.charAt(0).toUpperCase() + monthName.slice(1);
-      this.weeks = weeks;
+
+        const monthName = date.toLocaleString('ru-RU', { month: 'long' });
+        
+        this.calendarMonths.push({
+          monthTitle: monthName.charAt(0).toUpperCase() + monthName.slice(1),
+          monthNumber: selectedMonth.split('-')[1],
+          year: selectedMonth.split('-')[0],
+          weeks: weeks
+        });
+      });
+
+      this.highlightCalendarDates();
+    },
+    highlightCalendarDates() {
+      this.calendarMonths.forEach((month) => {
+        // if (month.monthNumber === '06') debugger
+        month.weeks.forEach((week) => {
+          for (let index = 0; index < week.length; index++) {
+            const day = week[index];
+            if (day === null) continue;
+            const currentDate = `${month.year}-${month.monthNumber}-${String(day).padStart(2, '0')}`;
+            
+            const matchingEvent = this.calendarEvents.find(
+              (event) => event.date === currentDate);
+
+            if (matchingEvent) {
+              week[index] = {
+                day: day,
+                highlighted: true,
+                event: matchingEvent.event,
+                name: matchingEvent.name
+              };
+            } else {
+              week[index] = {
+                day: day,
+                highlighted: false
+              };
+            }
+          }
+        });
+      });
+      this.events = this.calendarMonths[0].weeks.flat().filter((day) => day?.highlighted && day?.event);
+      this.weekend = this.calendarMonths[0].weeks.flat().filter((day) => day?.highlighted && !day?.event);
+    },
+    updateCardsInfo() {
+      console.log('ОБНОВИТЬ!')
+      const activeIndexOfSlide = this.$refs.swiper.swiperInstance.realIndex;
+      this.events = this.calendarMonths[activeIndexOfSlide].weeks.flat().filter((day) => day?.highlighted && day?.event);
+      this.weekend = this.calendarMonths[activeIndexOfSlide].weeks.flat().filter((day) => day?.highlighted && !day?.event);
     }
   }
 };
@@ -81,20 +443,20 @@ export default {
   <div>
     <div class="cards">
       <div class="cards__item event">
-        <span>2</span>
+        <span>{{ events.length }}</span>
         <span>Запланированные <br> события</span>
       </div>
       <div class="cards__item weekend">
-        <span>8</span>
+        <span>{{ weekend.length }}</span>
         <span>Выходные</span>
       </div>
     </div>
     <div class="navigation">
-      <swiper ref="swiper" :options="swiperOptions" class="swiper">
-        <swiper-slide v-for="item in 4" :key="item" class="swiper-slide">
+      <swiper ref="swiper" :options="swiperOptions" @slideChange="updateCardsInfo" class="swiper">
+        <swiper-slide v-for="item in calendarMonths" :key="item" class="swiper-slide">
           <div class="swiper-item">
             <div class="calendar">
-              <h2>{{ monthTitle }}</h2>
+              <h2>{{ item.monthTitle }}</h2>
               <table>
                 <thead>
                   <tr>
@@ -102,8 +464,15 @@ export default {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(week, index) in weeks" :key="index">
-                    <td v-for="(day, dayIndex) in week" :key="dayIndex" class="day-cell">{{ day }}</td>
+                  <tr v-for="(week, index) in item.weeks" :key="index">
+                    <td v-for="(day, dayIndex) in week" :key="dayIndex" class="day-cell"
+                      :style="{ 
+                        'color': day?.highlighted && day?.event ? '#6588F9' : day?.highlighted && !day?.event ? '#28DB7A' : '#6D6D6D',
+                        'cursor': day?.highlighted && day?.event ? 'pointer' : 'default'
+                      }"
+                    >
+                      {{ day?.day }}
+                    </td>
                   </tr>
                 </tbody>
               </table>
@@ -135,6 +504,7 @@ export default {
 }
 
 .calendar__arrow {
+  cursor: pointer;
   z-index: 1;
   position: absolute;
   top: 50%;
@@ -142,6 +512,7 @@ export default {
   height: 41px;
   border-radius: 50%;
   background-color: #fff;
+  transition: all 300ms ease-in;
 
   display: flex;
   align-items: center;
@@ -153,6 +524,11 @@ export default {
 
   &.next {
     right: 0;
+  }
+
+  &.swiper-button-disabled {
+    cursor: default;
+    opacity: 0.5;
   }
 }
 
@@ -202,6 +578,7 @@ export default {
   }
 }
 .calendar {
+  margin: 0 auto;
   font-family: Arial, sans-serif;
   width: 300px;
 }
