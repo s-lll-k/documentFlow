ee199aaf826eda31891ee6218edc1782f71ff3b2
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2023-06-06 23:51:37 +0600

    settings adaptive

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -2,24 +2,50 @@
 export default {
   data() {
     return {
-        
+
     };
   },
   async created() {
-    
+
   },
   methods: {
-    
+
   },
 };
 </script>
 
 <template>
   <div>
-    НОВОСТИ
+    <div class="faq">
+      <h1 class="faq__title">
+        Часто задаваемые вопросы
+      </h1>
+    </div>
+
   </div>
 </template>
 
 <style lang="scss" scoped>
+.faq {
+  &__title {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    // line-height: 40px;
+    color: #0c0b0b;
+    margin-bottom: 34px;
+    text-align: center;
+    margin-bottom: 150px;
 
+    @media screen and (max-width:1200px) {
+      font-size: 24px;
+      margin-bottom: 120px;
+    }
+
+    @media screen and (max-width:500px) {
+      font-size: 20px;
+      margin-bottom: 70px;
+    }
+  }
+}
 </style>
--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -44,37 +44,57 @@ export default {
 <template>
   <div>
     <!-- <nuxt-link class="твой класс" :to="'/faq'">
-                        внутрянка как и у обычных блоков
-                      </nuxt-link> -->
+                                                                внутрянка как и у обычных блоков
+                                                              </nuxt-link> -->
     <div class="settings">
       <h1 class="settings__title">
         Настройки
       </h1>
 
       <div class="settings__items">
-        <div class="settings__item">
+        <nuxt-link class="settings__item" :to="'/faq'">
           FAQ
-        </div>
-        <div class="settings__item">
-          <div class="filter">
-            <p class="filter__title" @click="switchItemsVisibility" v-click-outside="externalClick">
-              Фильтровать
-              <svg :class="{ reveal: show, closed: !show }" width="14" height="8" viewBox="0 0 14 8" fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 1L7 7L13 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-            </p>
-            <div v-show="show" class="filter__items">
-              <div class="filter__item" >
-                По новым
-              </div>
-              <div class="line"></div>
-              <div class="filter__item" >
-                По старым
-              </div>
+        </nuxt-link>
+        <div class="settings__item " @click="switchItemsVisibility" v-click-outside="externalClick">
+
+          Фильтровать
+          <svg :class="{ reveal: show, closed: !show }" width="14" height="8" viewBox="0 0 14 8" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L7 7L13 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <div v-show="show" class="filter__items">
+            <div class="filter__item">
+              По новым
+            </div>
+            <div class="line"></div>
+            <div class="filter__item">
+              По старым
             </div>
           </div>
+
         </div>
+      </div>
+
+      <div class="settings__socials">
+        <h2 class="settings__socials-title">Контакты</h2>
+        <ul class="settings__socials-block">
+          <li class="settings__socials-link"><a href="reception@iitu.edu.kz" target="_blank"
+              class="settings__socials-link">
+              <img src="../../assets/images/gmail.svg" alt="gmail" class="settings__socials-img">
+            </a></li>
+          <li class="settings__socials-link"><a href="https://www.instagram.com/iitu_kz/" target="_blank"
+              class="settings__socials-link">
+              <img src="../../assets/images/inst.svg" alt="inst" class="settings__socials-img">
+            </a></li>
+          <li class="settings__socials-link"><a href="https://vk.com/iitu_edu_kz" target="_blank"
+              class="settings__socials-link">
+              <img src="../../assets/images/vkontakte.svg" alt="vkontakte" class="settings__socials-img">
+            </a></li>
+          <li class="settings__socials-link"><a href="https://t.me/iitunews" target="_blank"
+              class="settings__socials-link">
+              <img src="../../assets/images/telegram.svg" alt="telegram" class="settings__socials-img">
+            </a></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -82,6 +102,16 @@ export default {
 
 <style lang="scss" scoped>
 .settings {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  min-height: 700px;
+  height: auto;
+
+  @media screen and (max-width:1200px) {
+    font-size: 24px;
+  }
+
   &__title {
     font-style: normal;
     font-weight: 700;
@@ -95,7 +125,71 @@ export default {
     }
 
     @media screen and (max-width:500px) {
-      font-size: px;
+      font-size: 20px;
+    }
+  }
+
+  &__socials {
+    border-top: 1px solid rgba(18, 18, 29, 0.1);
+    padding: 20px 0;
+    justify-self: flex-end;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: auto;
+
+    &-block {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0 20px;
+    }
+
+    &-title {
+      font-family: 'Segoe UI';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 24px;
+      // line-height: 32px;
+
+      margin-bottom: 15px;
+      color: #0C0B0B;
+    }
+  }
+
+  &__items {
+    display: flex;
+    justify-content: space-evenly;
+
+    @media screen and (max-width:680px) {
+      flex-direction: column;
+      gap: 20px 0;
+    }
+  }
+
+  &__item {
+    max-width: 253px;
+    width: 100%;
+    height: 74px;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.32);
+    border-radius: 14px;
+
+    text-decoration: none;
+    font-family: 'Segoe UI';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    // line-height: 32px;
+    text-align: center;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #0C0B0B;
+
+    @media screen and (max-width:680px) {
+      max-width: 100%;
     }
   }
 }
